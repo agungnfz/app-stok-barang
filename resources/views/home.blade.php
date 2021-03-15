e91a33aaa3452fb6131f039322e6519bf65d7018
agungnfz <agungnfz@gmail.com>
2021-03-15 12:26:39 +0700

    upload stok

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,7 +17,7 @@
             <div class="icon">
                 <i class="ion ion-bag"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            <a href="#" class="small-box-footer">More infos <i class="fa fa-arrow-circle-right"></i></a>
         </div>
     </div>
     <!-- ./col -->
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -50,7 +50,7 @@
         <nav class="navbar navbar-static-top" role="navigation">
             <!-- Sidebar toggle button-->
             <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
-                <span class="sr-only">Toggle navigation</span>
+                <span class="sr-only">Toggle navigations</span>
             </a>
             <!-- Navbar Right Menu -->
             <div class="navbar-custom-menu">
